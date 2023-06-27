143764812b0ea23fc5ba47f7397b00549669a40f
lais <laisziegler@gmail.com>
2023-06-27 16:06:52 -0300

    create screen for medicines list

--- medicare-frontend/src/app/list-medicine/medicine.ts
+++ medicare-frontend/src/app/list-medicine/medicine.ts
@@ -1,10 +1,11 @@
+import { Category } from "../sort-category/category";
 
 export class Medicine {
     
     id: number;
     name: string;
     // company: Company;
-    // category: Category;
+    category: Category;
     description: string;
     price: number;
 
